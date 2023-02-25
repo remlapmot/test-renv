1c8cef96709b27c13ccd44db0125c8482a03571b
Tom Palmer <remlapmot@hotmail.com>
2023-02-25 21:46:29 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,7 +6,7 @@ rm -r ~/.cache/R/renv
 rm -r ./renv/library/R-4.2/x86_64-pc-linux-gnu/
 R CMD BATCH before.R before-01.Rout
 rm .RData
-R CMD BATCH main.R
+R CMD BATCH main-01.R
 
 # Test 02
 rm .RData
