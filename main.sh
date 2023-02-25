69b86bba08378a6d44827e4f7b02c4becb05d0d0
Tom Palmer <remlapmot@hotmail.com>
2023-02-25 22:03:10 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,17 +1,31 @@
 #!/usr/bin/env bash
 
 # Test 01
+rm renv.lock
 rm .RData
 rm -r ~/.cache/R/renv
 rm -r ./renv/library/R-4.2/x86_64-pc-linux-gnu/
+rm .Rprofile
+rm -r ./renv/sandbox/
+rm -r ./renv/staging/
+rm ./renv/.gitignore
+rm ./renv/activate.R
+rm ./renv/settings.dcf
 R CMD BATCH before.R before-01.Rout
 rm .RData
 R CMD BATCH main-01.R
 
 # Test 02
+rm renv.lock
 rm .RData
 rm -r ~/.cache/R/renv
 rm -r ./renv/library/R-4.2/x86_64-pc-linux-gnu/
+rm .Rprofile
+rm -r ./renv/sandbox/
+rm -r ./renv/staging/
+rm ./renv/.gitignore
+rm ./renv/activate.R
+rm ./renv/settings.dcf
 R CMD BATCH before.R before-02.Rout
 rm .RData
 R CMD BATCH main-02.R
