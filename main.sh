fe98566dce285a8128d24d8a474186ccfa0f357b
Tom Palmer <remlapmot@hotmail.com>
2023-02-25 21:28:45 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
-R CMD BATCH before.R
+
+# Test 01
+rm -r ~/.cache/R/renv
+rm -r ./renv/library/R-4.2/x86_64-pc-linux-gnu/
+
+R CMD BATCH before.R before-01.Rout
 R CMD BATCH main.R
+
+# Test 02
+rm -r ~/.cache/R/renv
+rm -r ./renv/library/R-4.2/x86_64-pc-linux-gnu/
+
+R CMD BATCH before.R before-02.Rout
+R CMD BATCH main-02.R
